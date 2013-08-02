342e2d8bd0ef88465d6cb821327e991f9e24c3f7
Carlos Alexandro Becker <caarlos0@gmail.com>
2013-08-02 15:34:13 -0300

    wip...

--- web.js
+++ web.js
@@ -2,16 +2,17 @@ var express = require('express')
   , fs = require('fs');
 
 var app = express.createServer(express.logger());
+app.use(express.static(__dirname + '/assets'));
 
-app.get('/', function(request, response) {
+app.get('/', function (req, res) {
   // async is always better
   fs.readFile('index.html', function (err, text) {
-    response.setHeader('Content-Type', 'text/html');
-    response.end(text);
+    res.setHeader('Content-Type', 'text/html');
+    res.end(text);
   });
 });
 
 var port = process.env.PORT || 8080;
-app.listen(port, function() {
-  console.log("Listening on " + port);
+app.listen(port, function () {
+  console.log("Listening on %s...", port);
 });
